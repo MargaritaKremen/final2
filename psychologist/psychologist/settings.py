6a8da874740f25e11ec3fda25c28515f635000d7
Margarita <margo197721@gmail.com>
2024-01-14 16:39:11 +0000

    if no profile

--- psychologist/psychologist/settings.py
+++ psychologist/psychologist/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-22#d!9xg2+&ko@b8$1wchk7hd45+2%!264g8kueahg5&$cf2%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
